e6a524d204221bd62be765e4b04ffd53b1ddabb5
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-12-03 13:09:50 +0900

    Add context menu functionality to background.js

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
-setInterval(function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-            message: 'data',
-        });
-    })
-}, 1000);
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    "id": "sampleContextMenu",
+    "title": "Sample Context Menu",
+    "contexts": ["all"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId == "sampleContextMenu") {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {message: 'data'});
+    });
+  }
+});
